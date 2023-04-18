a110a3bf7120a6aad05316a9fce2f02fac1369f1
xincheng <106133510+281120839@users.noreply.github.com>
2023-04-18 19:55:34 +0800

    Update constant.ts

--- app/constant.ts
+++ app/constant.ts
@@ -1,8 +1,8 @@
-export const OWNER = "Yidadaa";
-export const REPO = "ChatGPT-星辰网";
+export const OWNER = "281120839";
+export const REPO = "ChatGPT-XC";
 export const REPO_URL = `https://zxxm.xyz`;
 export const ISSUE_URL = `https://zxxm.xyz`;
-export const UPDATE_URL = `${REPO_URL}#keep-updated`;
+export const UPDATE_URL = `https://xc.nncv.xyz`;
 export const FETCH_COMMIT_URL = `https://api.github.com/repos/${OWNER}/${REPO}/commits?per_page=1`;
 export const FETCH_TAG_URL = `https://api.github.com/repos/${OWNER}/${REPO}/tags?per_page=1`;
 export const RUNTIME_CONFIG_DOM = "danger-runtime-config";
